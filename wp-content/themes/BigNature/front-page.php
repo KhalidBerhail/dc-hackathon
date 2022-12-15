bd9f6b15b8f9a02abfe9b422b192edbffc4c97ba
Kurolo <khalidbe3363@gmail.com>
2022-12-16 00:39:41 +0100

    style: mobile optimisations

--- wp-content/themes/BigNature/front-page.php
+++ wp-content/themes/BigNature/front-page.php
@@ -9,8 +9,25 @@ $pro2 = get_field('2e_produit_alimentaire');
 $pro3 = get_field('3e_produit_alimentaire');
 $list_prods= array($pro1 ,$pro2,$pro3);
 $metric = get_field('metric_a_afficher');
+$metric_name = $metric["label"];
+$metric_value = $metric["value"];
+$metric_unit = 'coucou';
 
-$pre_url = "https://data.ademe.fr/data-fair/api/v1/datasets/agribalyse-synthese/metric_agg?metric=avg&field=".urlencode($metric);
+// Truncate unité
+$metric_unit_truncated = $metric_value;
+$ini = strpos($metric_unit_truncated, "(") + 1;
+$fin = strpos($metric_unit_truncated, ")");
+if ($ini != 0){
+	// $ini += strlen("(");
+	// $len = strpos($metric_unit_truncated, ")", $ini) - $ini
+}
+$metric_unit_truncated = substr($metric_unit_truncated, $ini, $fin - $ini);
+
+// var_dump($metric_unit_truncated);
+// var_dump($ini);
+// var_dump($fin);
+
+$pre_url = "https://data.ademe.fr/data-fair/api/v1/datasets/agribalyse-synthese/metric_agg?metric=avg&field=".urlencode($metric_value);
 
 ?>
 
@@ -82,6 +99,8 @@ $pre_url = "https://data.ademe.fr/data-fair/api/v1/datasets/agribalyse-synthese/
 			</h1>
 		</section><!-- #hero-section -->
 		
+		<!-- DATA PROJET SECTION -->
+		
 		<section id="data-section" class="data-section">
 			<div class="data-section-header data-header">
 				
@@ -99,7 +118,7 @@ $pre_url = "https://data.ademe.fr/data-fair/api/v1/datasets/agribalyse-synthese/
 				<span class="data-header__rec"></span>
 			</div>
 
-			<div class="data-section-content container">
+			<div class="data-section-intro">
 				<?php 
 					$data_logo = get_field('logo_section_projet');
 					if($data_logo) :
@@ -121,127 +140,81 @@ $pre_url = "https://data.ademe.fr/data-fair/api/v1/datasets/agribalyse-synthese/
 				<?php
 					endif;
 				?>
-				<div class="data-content__stats"></div>
-				
 			</div>
-			
-			<?php 
+
+			<div class="data-section-container">
+				<?php 
 				$data_image = get_field('image_section_projet');
 				if($data_image) :
-			?>
+				?>
 					<div class="data-section__tree">
 						<img src="<?php echo $data_image['url'];?>" alt="">
 					</div>
-			<?php
+				<?php
 				endif;
-			?>
+				?>
+
+				<div class="data-content__stats">
+
+					<?php
+						foreach($list_prods as $prod):
+							$product = $prod['label'];
+							$url_fetch = $pre_url."&q=".urlencode($product)."&q_mode=simple";
+							$ch = curl_init();
+							
+							try {
+								curl_setopt($ch, CURLOPT_URL, $url_fetch);
+								curl_setopt($ch, CURLOPT_HEADER, false);
+								curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+								curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);   
+								curl_setopt($ch, CURLOPT_TIMEOUT, 5);         
+								curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+								curl_setopt($ch, CURLOPT_MAXREDIRS, 1);
+								curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+						
+								
+								$response = curl_exec($ch);
+								
+							} catch (\Throwable $th) {
+								throw $th;
+							} finally {
+								if (curl_errno($ch)) {
+									echo curl_error($ch);
+									die();
+								}
+								
+							}
+							$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+							if($http_code == intval(200)) :
+								//decoding json response
+								$obj = json_decode($response);
+								$obj_number = number_format($obj->metric, 3, '.', '');
+								if($obj_number)
+					?>
+									<div class="data-element">
+										<h3 class="data-element__title"><?php echo $product?></h3>
+										<p class="data-element__num"> ~ <?php echo $obj_number;?></p>
+										<p class="data-element__desc"><?php echo $metric_name;?></p>
+										<p class="data-element__unit"><?php echo $metric_unit_truncated;?></p>
+									</div>
+					<?php
+							
+							else:
+								echo "problem with retrieving data";
+							endif;
+						endforeach;
+					?>
+
+				</div>
+			</div>
 		</section>
+
+		<!-- FIN DATA PROJET SECTION -->
+		
 		<div class="img-separator">
 			<img src="<?php echo get_template_directory_uri();?>/images/jpg/wheat-field.jpg" alt="">
 		</div>
-		<!--<div class="data-section">
-			<?php
-				foreach($list_prods as $prod):
-					$product = $prod['label'];
-					$url_fetch = $pre_url."&q=".urlencode($product)."&q_mode=simple";
-					$ch = curl_init();
-					
-					try {
-						curl_setopt($ch, CURLOPT_URL, $url_fetch);
-						curl_setopt($ch, CURLOPT_HEADER, false);
-						curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-						curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);   
-						curl_setopt($ch, CURLOPT_TIMEOUT, 5);         
-						curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-						curl_setopt($ch, CURLOPT_MAXREDIRS, 1);
-						curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-				
-						
-						$response = curl_exec($ch);
-						
-					} catch (\Throwable $th) {
-						throw $th;
-					} finally {
-						if (curl_errno($ch)) {
-							echo curl_error($ch);
-							die();
-						}
-						
-					}
-					$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-					if($http_code == intval(200)) :
-						//decoding json response
-						$obj = json_decode($response);
-				
-			?>
-						<div class="data-element">
-							<h3 class="data-element__title"><?php echo $product?></h3>
-							<p class="data-element__metric">
-								<strong><?php echo $metric.":";?></strong>
-								<?php echo $obj->metric;?>
-							</p>
-						</div>
-			<?php
-					
-					else:
-						echo "problem with retrieving data";
-					endif;
-				endforeach;
-			?>
-		</div>-->
-		<!-- #data-section -->
-
-		<!--<div class="horizontal-section engagement engagement-section">
-			<div class="engagement-back__img">
-				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="790.454" height="1040.366" viewBox="0 0 790.454 1040.366">
-					<defs>
-						<filter id="Icon_awesome-leaf" x="0" y="0" width="790.454" height="1040.366" filterUnits="userSpaceOnUse">
-						<feOffset dy="1" input="SourceAlpha"/>
-						<feGaussianBlur stdDeviation="1.5" result="blur"/>
-						<feFlood flood-opacity="0.161"/>
-						<feComposite operator="in" in2="blur"/>
-						<feComposite in="SourceGraphic"/>
-						</filter>
-					</defs>
-					<g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#Icon_awesome-leaf)">
-						<path id="Icon_awesome-leaf-2" data-name="Icon awesome-leaf" d="M933.013,13.567c-9.567-17.443-36.9-18.141-48.346-1.675-52.959,75.214-147.773,122.1-256.082,122.1H491.916c-181.085,0-328,120.008-328,267.924,0,9.768,1.367,19.117,2.563,28.606,108.993-63.772,266.332-117.914,489.443-117.914,15.033,0,27.334,10.047,27.334,22.327s-12.3,22.327-27.334,22.327C226.438,357.263,44.328,572.3,4.01,653.1c-11.275,22.746,2.05,48.7,29.9,58.05,28.017,9.489,59.792-1.535,71.409-24.141,2.563-5.024,35.7-66.842,122.831-126.427,55.351,61.26,160.585,119.729,298.791,107.728C795.148,652.4,983.922,455.921,983.922,215.347c0-70.051-18.45-142.614-50.909-201.78Z" transform="translate(717.32 3.5) rotate(86)" fill="#dadfab" opacity="0.336"/>
-					</g>
-				</svg>
-			</div>
-			<div class="horizontal-container container">
-				<div class="engagement-intro">
-					<h2 class="engagement-intro__title">
-						nos engagements
-					</h2>
-					<p class="engagement-intro__text">
-						découvrez nos engagements 2022 et nos ambitions pour les années à venir.
-					</p>
-				</div>
-				<div class="horizontal-scroll 2022-scroll">
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-					<div class="horizontal-scroll__item"></div>
-				</div>
-			</div>
-		</div>-->
 
 		<div class="horizontal">
       		<div id="panels" class="container horizontal-container horizontal-section engagement engagement-section">
@@ -326,6 +299,47 @@ $pre_url = "https://data.ademe.fr/data-fair/api/v1/datasets/agribalyse-synthese/
       		</div>
 		</div>
 
+		<div class="horizontal-mobile">
+			<div class="horizontal-mobile-header">
+				<h2 class="engagement-intro__title">
+					Nos engagement
+				</h2>
+			</div>
+			<div class="horizontal-mobile-grid">
+				
+				<?php 
+					if($engagements) : 
+						$i=0;
+						while($i<$eng_count) :
+							$i++;
+							$img_key = 'image_engagement_'.$i;
+							$img_url = $engagements[$img_key]['url'];
+							$eng_desc_field = 'description_engagement_'.$i;
+							$eng_title_field = 'titre_engagement_'.$i;
+							$eng_date_field = 'date_engagement_'.$i;
+							$eng_title = $engagements[$eng_title_field];
+							$eng_desc = $engagements[$eng_desc_field];
+							$eng_date = $engagements[$eng_date_field];
+				?>
+				
+					<div class="horizontal-mobile__card">
+						<div class="horizontal-mobile__card--img">
+							<img src="<?php echo esc_url($img_url);?>" alt="">
+						</div>
+						<span><?php echo $eng_date;?></span>
+						<p class="horizontal-mobile__text">
+							<?php echo $eng_desc;?>
+						</p>
+					</div>
+				<?php
+						endwhile;
+					endif;
+				?>
+			</div>
+		</div>
+
+		
+
 		
 
 		<!-- -------------- OUR TREES --------------  -->
@@ -341,20 +355,12 @@ $pre_url = "https://data.ademe.fr/data-fair/api/v1/datasets/agribalyse-synthese/
 			</div>
 			<div class="ourtrees-map">
 				<img class="ourtrees-carte" src="<?php echo get_template_directory_uri();?>/images/png/carte.png" alt="foret"" alt="Carte de France">
-				<div class="ourtrees-legend">
-					<div class="ourtrees-legend-container">
-						<img class="ourtrees-legend-leaf" src="<?php echo get_template_directory_uri();?>/images/png/white_leaf.png" alt="feuille blanche">
-						<span class="ourtrees-legend-text">arbres plantés</span>
-					</div>
-					<div class="ourtrees-legend-container">
-						<img class="ourtrees-legend-leaf" src="<?php echo get_template_directory_uri();?>/images/png/green_leaf.png" alt="feuille verte">
-						<span class="ourtrees-legend-text">future plantation</span>
-					</div>
-				</div>
 			</div>
 		</section>
+		
 
 
+		<!-- -------------- JOIN US --------------  -->
 		<!-- -------------- JOIN US --------------  -->
 		<section class="joinus-section joinus">
 			<div class="joinus-box">
@@ -371,15 +377,16 @@ $pre_url = "https://data.ademe.fr/data-fair/api/v1/datasets/agribalyse-synthese/
 					<div>
 						<div class="joinus-highlight"> </div>
 						<div class="joinus-text">rejoignez-nous !</div>
+						<?php echo do_shortcode("[formidable id=2]"); ?>
 					</div>
 					<img class="site-footer-illustration mobile-ble" src="<?php echo get_template_directory_uri();?>/images/png/ble.png" alt="blé">
 				</div>
 
-				<div class="joinus-box-three">
+				<!-- <div class="joinus-box-three">
 					<button class="joinus-button">
 						devenir membre
 					</button>
-				</div>
+				</div> -->
 			</div>
 		</section>
 
